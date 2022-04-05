99c745aa456f6c1e1c39cad1ef0c6ebcd7c45141
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2022-04-05 16:35:37 +0545

    Leetcode: Remove duplicates from array

---
+++ leetcode/26-remove-duplicates.js
@@ -0,0 +1,14 @@
+const removeDuplicates = (nums) => {
+  let i = 0;
+
+  while (i < nums.length) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i + 1, 1);
+    } else {
+      i++;
+    }
+  }
+
+  console.log(nums);
+  return nums.length;
+};
