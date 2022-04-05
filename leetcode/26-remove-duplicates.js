9e8dd05e9ac232c0777a71d64b5ef59e1a58a627
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2022-04-05 16:55:25 +0545

    Leetcode: Remove duplicates from array - updated solution

--- leetcode/26-remove-duplicates.js
+++ leetcode/26-remove-duplicates.js
@@ -1,14 +1,32 @@
-const removeDuplicates = (nums) => {
-  let i = 0;
+// const removeDuplicates = (nums) => {
+//   let i = 0;
 
-  while (i < nums.length) {
-    if (nums[i] === nums[i + 1]) {
-      nums.splice(i + 1, 1);
-    } else {
-      i++;
+//   while (i < nums.length) {
+//     if (nums[i] === nums[i + 1]) {
+//       nums.splice(i + 1, 1);
+//     } else {
+//       i++;
+//     }
+//   }
+
+//   return nums.length;
+// };
+
+// Solution from neetcode
+// This solution is faster
+const removeDuplicates = (nums) => {
+  let left = 1;
+
+  for (let right = 1; right < nums.length; right++) {
+    if (nums[right] !== nums[right - 1]) {
+      nums[left] = nums[right];
+      left++;
     }
   }
 
-  console.log(nums);
-  return nums.length;
+  return left;
 };
+
+let nums = [1, 1, 2];
+
+removeDuplicates(nums);
