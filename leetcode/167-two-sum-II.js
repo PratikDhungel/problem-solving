f445b82073fec488a98532e0b63b5a866bc5d5f1
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2022-05-09 22:50:55 +0545

    Leetcode: Two sum problem part 2 - updated solution

--- leetcode/167-two-sum-II.js
+++ leetcode/167-two-sum-II.js
@@ -1,15 +1,37 @@
+// const twoSum = (nums, target) => {
+//   const map = new Map();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const currValue = nums[i];
+//     const diff = target - currValue;
+//     const diffValue = map.get(diff);
+
+//     if (diffValue !== undefined) {
+//       return [diffValue, i + 1];
+//     } else {
+//       map.set(currValue, i + 1);
+//     }
+//   }
+// };
+
+// Updated solution, since input array is sorted
+// User two pointers
 const twoSum = (nums, target) => {
-  const map = new Map();
+  let left = 0;
+  let right = nums.length - 1;
 
-  for (let i = 0; i < nums.length; i++) {
-    const currValue = nums[i];
-    const diff = target - currValue;
-    const diffValue = map.get(diff);
+  while (left < right) {
+    const leftValue = nums[left];
+    const rightValue = nums[right];
 
-    if (diffValue !== undefined) {
-      return [diffValue, i + 1];
-    } else {
-      map.set(currValue, i + 1);
+    const currSum = leftValue + rightValue;
+
+    if (currSum === target) return [left + 1, right + 1];
+    if (currSum > target) {
+      right--;
+    }
+    if (currSum < target) {
+      left++;
     }
   }
 };
